5c7c3da24eaad0ffc2579259ea7bcad4187aad82
Kenoh7 <97468788+Kenoh7@users.noreply.github.com>
2022-06-08 15:00:56 -0700

    Project
    Project

---
+++ domosushi/src/main/java/com/kenoh/domosushi/DomosushiApplication.java
@@ -0,0 +1,12 @@
+package com.kenoh.domosushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomosushiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomosushiApplication.class, args);
+	}
+}
